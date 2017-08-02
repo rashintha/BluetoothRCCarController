56b4113c346f64ae4adccc42aa5b6cc40b060a72
Rashintha Maduneth <rashinthamaduneth@gmail.com>
2017-08-02 21:43:01 +0530

    Bluetooth enable is working.

--- app/src/main/java/com/rashintha/opensource/bluetoothrccarcontroller/MainActivity.java
+++ app/src/main/java/com/rashintha/opensource/bluetoothrccarcontroller/MainActivity.java
@@ -1,17 +1,40 @@
 package com.rashintha.opensource.bluetoothrccarcontroller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //abc
-        String a = "", b;
-        int ab;
+        ImageButton btnStop = (ImageButton) findViewById(R.id.btnStop);
+        ImageButton btnUp = (ImageButton) findViewById(R.id.btnUp);
+        ImageButton btnDown = (ImageButton) findViewById(R.id.btnDown);
+        ImageButton btnLeft = (ImageButton) findViewById(R.id.btnLeft);
+        ImageButton btnRight = (ImageButton) findViewById(R.id.btnRight);
+        ImageButton btnBluetoothEnable = (ImageButton) findViewById(R.id.btnBluetoothEnable);
+        ImageButton btnBluetoothConnect = (ImageButton) findViewById(R.id.btnBluetoothConnect);
+        ImageButton btnBluetoothDisconnect = (ImageButton) findViewById(R.id.btnBluetoothDisconnect);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnBluetoothEnable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!bluetoothAdapter.isEnabled()){
+                    Intent enable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enable, 0);
+                }
+            }
+        });
     }
 }
